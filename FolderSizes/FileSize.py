8d2f14a7bb4fc169de029e7666ab91af34407426
IcarusVi <zscyrus31@gmail.com>
2021-03-29 19:59:23 -0400

    Updated DirectoryManager class

--- FolderSizes/FileSize.py
+++ FolderSizes/FileSize.py
@@ -2,53 +2,43 @@
 # and show them all their folders;
 import os
 
-#choose a drive 
+# choose a drive
 driveLetter = input('Enter drive letter: ')
 
-drive = driveLetter + ':/'
+driveName = driveLetter + ':/'
 
-#Function to get directory count of chosen drive
-def getDirectoryCount(location):
-    if os.path.exists(location):
-        count = 0
-        allDirectories = os.listdir(location)
-        for name in allDirectories:
-            count = count+1
-        fileAndFolder = ("File and Folder count: %d" %count)
-        return fileAndFolder
-    else:
-        return "Drive doesn't exist or improper format"
-
-    
-#Function to list all folders/directories within drive 
-def listAllDirectories(location):
-    if os.path.exists(location):
-        allDirectories = os.listdir(location)
-        return allDirectories
-    else:
-        return "Drive doesn't exist or improper format"
-
-
-#Building a class to handle directories
-#Class will use the above functions to find directories
-#Might be a good idea to determine a root folder
+# Building a class to handle directories
+# Class will use the above functions to find directories
+# Might be a good idea to determine a root folder
 class DirectoryManager:
-    def __init__(dir, root):
-        self.root = root
+    def __init__(self, dir, drive):
+        self.drive = drive
         self.dir = dir
-        
-    #Function to get directory count of chosen drive
-    def getDirectoryCount():
-    if os.path.exists(self.root):
-        count = 0
-        allDirectories = os.listdir(location)
-        for name in allDirectories:
-            count = count+1
-        fileAndFolder = ("File and Folder count: %d" %count)
-        return fileAndFolder
-    else:
-        return "Drive doesn't exist or improper format"
+
+    # Function to get directory count of chosen drive
+    def getDirectoryCount(self):
+        if os.path.exists(self.drive):
+            count = 0
+            allDirectories = os.listdir(self.drive)
+            for name in allDirectories:
+                count = count+1
+            fileAndFolder = ("File and Folder count: %d" % count)
+            return fileAndFolder
+        else:
+            return "Drive doesn't exist or improper format"
+
+    # Function to list all folders/directories within drive
+    def listAllDirectories(self):
+        if os.path.exists(self.drive):
+            allDirectories = os.listdir(self.drive)
+            return allDirectories
+        else:
+            return "Drive doesn't exist or improper format"
 
 
-print(listAllDirectories(drive))
-print(getDirectoryCount(drive))
+
+
+
+dDrive = DirectoryManager('', driveName)
+print(dDrive.getDirectoryCount())
+print(dDrive.listAllDirectories())
